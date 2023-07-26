5a2e4f762a63741ec010bc63cbcea3d8486f93df
orangingq <orangingq@kaist.ac.kr>
2023-07-26 13:35:46 +0900

    environment added

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python3 run.py \
-    --kg_type=RDF-star \
-    --prompt_type=None \
-    --example_type=1 \
-    --input_type=0 \
-    --model_type=KULLM;
-
-# for ((i=1; i<=5; i++))
-# do
-#     python3 run.py \
+# python3 run.py \
 #     --kg_type=RDF-star \
 #     --prompt_type=None \
 #     --example_type=1 \
 #     --input_type=0 \
 #     --model_type=KULLM;
-# done
+
+for ((i=1; i<=5; i++))
+do
+    python3 run.py \
+    # --kg_type=RDF-star \
+    # --prompt_type=None \
+    # --example_type=1 \
+    # --input_type=0 \
+    --model_type=LLAMA2;
+done
 
 # tmux attach -t orangingq
 # ctrl+B -> D
